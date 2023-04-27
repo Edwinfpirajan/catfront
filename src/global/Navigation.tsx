90607b3658afe64f7d27bac6294c560db6bba4f5
Edwinfpirajan <efpa1998@hotmail.com>
2023-04-27 00:03:59 -0500

    add navbar

---
+++ src/global/Navigation.tsx
@@ -0,0 +1,30 @@
+const Navigation = () => {
+    return (
+        <nav className="flex justify-between items-center bg-transparent p-6 px-20">
+            <div className="flex items-center">
+                <img src="" className="w-10 h-10 mr-2" alt="logo" />
+                <span className="font-semibold text-xl tracking-tight">My App</span>
+            </div>
+            <div className="w-full block lg:flex lg:items-center lg:w-auto">
+                <div className="text-sm lg:flex-grow flex justify-between w-full">
+                    <a
+                        href="/"
+                        className="block mt-4 lg:inline-block lg:mt-0 text-gray-400 hover:text-gray-800 font-medium transition duration-700 mr-4 text-lg"
+                        style={{ fontSize: "1.2rem" }}
+                    >
+                        Home
+                    </a>
+                    <a
+                        href="/cat"
+                        className="block mt-4 lg:inline-block lg:mt-0 text-gray-400 hover:text-gray-800 font-medium transition duration-700 mr-4 text-lg"
+                        style={{ fontSize: "1.2rem" }}
+                    >
+                        Cat
+                    </a>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navigation;
